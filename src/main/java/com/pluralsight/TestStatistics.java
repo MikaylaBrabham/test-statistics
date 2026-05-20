17b2f656a4b76cebc1b1d0bbbec4097c325424c1
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 02:46:37 -0500

    initial array and psudo code

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
 public class TestStatistics {
+
+    //add main
+    public static void main(String[] args) {
+
+        //create an array of 10 test scores
+        int[] testScores = {12, 78, 56, 50, 90, 71, 84, 110, 2, 38};
+
+        //add the logic
+
+        //print out the average
+
+        //print out thr high score
+
+        //print out the low score
+
+        //print out the median
+
+
+    }
 }
