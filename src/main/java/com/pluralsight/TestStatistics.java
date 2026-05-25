a8277bbc13949c339e7ff16eb5ee1f38cd29ed02
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-24 23:43:30 -0500

    added string to print out

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -1,5 +1,8 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+import java.io.*;
+
 public class TestStatistics {
 
     //add main
@@ -8,16 +11,39 @@ public class TestStatistics {
         //create an array of 10 test scores
         int[] testScores = {12, 78, 56, 50, 90, 71, 84, 110, 2, 38};
 
-        //add the logic
+        //initial scores
+        int sum = 0;
+        int highScore = testScores[0];
+        int lowScore = testScores[0];
 
-        //print out the average
+        //add the logic in for loop
+        for (int testScore : testScores) {
+            sum += testScore;
 
-        //print out thr high score
+            //print out the high score
+            if (testScore > highScore) {
+            highScore = testScore;
+               // System.out.println("The Highscore is " + highScore);
+            }
 
-        //print out the low score
+            //print out the low score
+            if (testScore < highScore) {
+                lowScore = testScore;
+               // System.out.println("The Lowscore is " + lowScore);
+            }
+
+        }
+        //average
+        double average = (double) sum /testScores.length;
 
         //print out the median
+        Arrays.sort(testScores);
+        double median = (testScores[4] + testScores[5]) / 2.0;
 
+        System.out.println("The average is " + average);
+        System.out.println("The highest score is " + highScore);
+        System.out.println("The lowest score is "+ lowScore);
+        System.out.println("The median is "+ median);
 
     }
 }
